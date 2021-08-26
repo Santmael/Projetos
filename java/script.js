057ab1dd9cfb156e419e71004b19614ba81d1dc6
Ismael Santos <snt.ismael@gmail.com>
2021-08-26 17:57:30 -0300

    Arquivos em JS e HTML
    Adicionei alguns arquivos de JS para treinamento e testando o GITHUB ao mesmo tempo.

---
+++ java/script.js
@@ -0,0 +1,14 @@
+
+var divHora = window.document.getElementById("divHora")
+var back = window.document.getElementsByClassName("body")
+var 
+var nome = window.prompt("diga seu nome")
+ var nomeR = "Renata"
+ 
+ if (nome==nomeR){
+    divHora.innerHTML ="Parabéns Mozão :D"
+ }else (
+        alert(" voce não é renata, lllfique apenas com as fotos")
+        (back.style.background = "#302e55")
+    )
+ 
