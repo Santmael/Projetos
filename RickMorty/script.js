62cb7b07f37d11e829c5068cbbbc6b5344ee1b2b
Ismael Santos <snt.ismael@gmail.com>
2022-06-15 01:18:01 -0300

    versão 1.0

--- RickMorty/script.js
+++ RickMorty/script.js
@@ -1,7 +1,25 @@
-alert('entrou no script')
-divt = window.document.querySelector('div')
-
-let texto = 'ola mundo'
 
 
-divt.innerText = `${texto} `
+async function dados (){
+
+    const url = await fetch('https://rickandmortyapi.com/api/character')
+    .then(Response => Response.json())
+
+    .then(function(json){
+        const persona = json.results.map(function(persona){
+            
+            const itens = document.querySelector('.itens')
+
+            itens.innerHTML += `<div class="conteudoapi"> 
+            <img src='${persona.image}'/> <hr>
+            <label> Nome: ${persona.name} <br>
+            <label>  Local: ${persona.origin.name}<br>
+            <label>  Status: ${persona.status}<br>
+            </div>`
+            
+        })
+        
+    })
+
+}
+dados();
